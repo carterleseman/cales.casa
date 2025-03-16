489932361df7b37ab7b69ee303ba9add99b06ecf
carterleseman <carter.leseman@outlook.com>
2025-03-16 14:43:50 -0500

    add flask infra

---
+++ routes/main.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, redirect, url_for
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return redirect(url_for('main.server'))
+
+@main.route('/server')
+def server():
+    return render_template('server.html')
